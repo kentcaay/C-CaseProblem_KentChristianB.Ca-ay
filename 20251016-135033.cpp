26b015134f7c66f4d003b63bdc94c183282e8489
kentcaay <kentcaay04@gmail.com>
2025-10-16 13:52:42 +0800

    Add files via upload

---
+++ 20251016-135033.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    // Display personal information
+    printf("Name: Kent Christian B. Ca-ay\n");
+    printf("ID Number: 2025304334\n");
+    printf("------------------------------\n");
+
+    int num1 = 10, num2 = 20, sum;
+
+    sum = num1 + num2;
+
+    printf("Result: The sum of %d and %d is %d\n", num1, num2, sum);
+
+    return 0;
+}
